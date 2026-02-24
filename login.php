02361917ca7f09bb7f366b53d709dcd41752f2b3
Andriu <goyajuanandres11@gmail.com>
2026-02-23 20:28:43 -0500

    Login terminado exitosamente

--- login.php
+++ login.php
@@ -2,3 +2,10 @@
 $saludo = "Hola usuario como estas?";
 echo = $saludo ;
 echo "V2 ";
+?>
+
+<form action="" method="post">
+    <label for="nombre">Nombre</label>
+    <input type="text">Ingrese su Nombre
+    <input type="submit" value="Subir">
+</form>
